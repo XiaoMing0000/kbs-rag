2929182cf44a15816ea7a62e714eea43415ef258
xiaoming0000 <1925952439@qq.com>
2026-08-23 19:31:59 +0800

    fmt: 移除 eslint、prettier，使用 oxlint、oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'oxfmt';
+import type { OxfmtConfig } from 'oxfmt';
+
+export default defineConfig({
+	printWidth: 150, // 每行最大字符数
+	useTabs: true, // 是否使用制表符
+	tabWidth: 4, // 缩进空格数
+	semi: true, // 语句末尾是否加分号
+	singleQuote: true, // 是否使用单引号
+	quoteProps: 'as-needed', // 对象的键是否使用引号
+	bracketSpacing: true, // 对象字面量括号内空格
+	trailingComma: 'all', // 是否使用尾逗号
+	sortPackageJson: false, // 是否排序 package.json 文件
+	endOfLine: 'lf', // 换行符
+	ignorePatterns: ['node_modules', 'dist', 'logs', 'pnpm-lock.json'],
+} satisfies OxfmtConfig);
